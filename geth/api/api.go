7d602805c527ab1092ef6114889536e0c0147c8d
Eugene <6655321@bk.ru>
2017-10-12 17:31:39 +0300

    Return error for notify

--- geth/api/api.go
+++ geth/api/api.go
@@ -191,7 +191,7 @@ func (api *StatusAPI) JailBaseJS(js string) {
 }
 
 // Notify and send message.
-func (api *StatusAPI) Notify(token string) string {
+func (api *StatusAPI) Notify(token string) error {
 	log.Debug("Notify", "token", token)
 
 	// TODO(oskarth): Experiment with this
@@ -200,10 +200,10 @@ func (api *StatusAPI) Notify(token string) string {
 		"sum": "Happy Day",
 	}
 
-	err := api.b.notification.Notify(msg, token)
+	err := api.b.notifier().Notify(msg, token)
 	if err != nil {
 		log.Error("Notify failed:", err)
 	}
 
-	return token
+	return err
 }
